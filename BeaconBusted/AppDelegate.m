61195ae3b0337605d902d196624010256aa946da
Bill Burgess <bill@simplymadeapps.com>
2019-01-16 13:28:11 -0600

    Fix location permission notification

--- BeaconBusted/AppDelegate.m
+++ BeaconBusted/AppDelegate.m
@@ -48,6 +48,9 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
 	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+	
+	// notify any location listeners the app has resumed
+	[[NSNotificationCenter defaultCenter] postNotificationName:@"WaitingOnAuthorizationStatus" object:nil];
 }
 
 
